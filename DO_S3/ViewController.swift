0a35746f46b01fef63a4dd9b83e9db025c72c771
Ler Tor <0079036184882@mail.ru>
2021-10-14 12:09:51 +0300

    Initial Commit

---
+++ DO_S3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DO_S3
+//
+//  Created by Andrey on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
